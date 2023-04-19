638358ffcccf9749ccdfe02519c301f31b150378
Samuel Chou <samuelchou@hotmail.com.tw>
2023-04-19 11:18:37 +0800

    feat(board): squares can be clicked and marked.

--- src/App.js
+++ src/App.js
@@ -11,28 +11,33 @@ function Square({ value, onHandleClick }) {
 }
 
 export default function Board() {
-  const [toggled, setToggle] = useState('');
-  const onHandleClick = (() => {
-    if (toggled === '') setToggle('O');
-    else setToggle('');
-  });
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  function onClickSquare(position) {
+    if (squares.length <= position) return; // TODO: make warning?
+    if (squares[position] === '') {
+      const newValues = squares.slice();
+      newValues[position] = 'X'; // TODO: make it into different player
+      setSquares(newValues);
+    } 
+  }
 
   return (
     <div>
       <div className="board-row">
-        <Square value={toggled} onHandleClick={onHandleClick} />
-        <Square value={toggled} onHandleClick={onHandleClick} />
-        <Square value={toggled} onHandleClick={onHandleClick} />
+        <Square value={squares[0]} onHandleClick={() => onClickSquare(0)} />
+        <Square value={squares[1]} onHandleClick={() => onClickSquare(1)} />
+        <Square value={squares[2]} onHandleClick={() => onClickSquare(2)} />
       </div>
       <div className="board-row">
-        <Square value={toggled} onHandleClick={onHandleClick} />
-        <Square value={toggled} onHandleClick={onHandleClick} />
-        <Square value={toggled} onHandleClick={onHandleClick} />
+        <Square value={squares[3]} onHandleClick={() => onClickSquare(3)} />
+        <Square value={squares[4]} onHandleClick={() => onClickSquare(4)} />
+        <Square value={squares[5]} onHandleClick={() => onClickSquare(5)} />
       </div>
       <div className="board-row">
-        <Square value={toggled} onHandleClick={onHandleClick} />
-        <Square value={toggled} onHandleClick={onHandleClick} />
-        <Square value={toggled} onHandleClick={onHandleClick} />
+        <Square value={squares[6]} onHandleClick={() => onClickSquare(6)} />
+        <Square value={squares[7]} onHandleClick={() => onClickSquare(7)} />
+        <Square value={squares[8]} onHandleClick={() => onClickSquare(8)} />
       </div>
     </div>
   );
